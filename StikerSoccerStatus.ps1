ba8013dc99482ffd449d2afd50ff38a547f1b66d
DynamicThought <57474094+DynamicThought@users.noreply.github.com>
2019-11-07 00:19:18 -0500

    Add files via upload

---
+++ StikerSoccerStatus.ps1
@@ -0,0 +1,28 @@
+﻿#Install-Module -Name PSParseHTML	
+
+
+#get the webpage
+$html = (Invoke-WebRequest -Uri "https://www.soccerinorlando.com/wed-4v4-schedule-results").content
+
+$schedule = ConvertFrom-HtmlTable -Content $html
+
+#$schedule[0] | Format-Table -AutoSize
+
+#loop through each game in the schedule
+foreach ($game in $schedule[0]){
+
+    if ($game -match 'wk'){
+        $week = $game.Time
+    }
+    if ($game -match 'Plan b'){
+
+        if($game.'Team 1' -notmatch 'plan B'){
+            $opponent = $game.'Team 1'
+        }
+        else{
+            $opponent = $game.'Team 2'
+        }
+
+        $week + " " + $game.Time + ' Field ' + $game.Field + ' vs. ' + $opponent
+    }
+}
